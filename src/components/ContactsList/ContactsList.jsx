3c257e3cf9dfa5445a21edc0990e660f3068f2bd
Darya <daryabobur@gmail.com>
2022-10-22 22:22:06 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -2,16 +2,16 @@ import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { useDispatch } from 'react-redux';
 import { Contacts } from './ContactsListStyled';
-import { deleteContact } from '../../redux/operations'
+import { deleteContact } from '../../redux/contacts/operations'
 import { BsFillTrashFill, BsPersonCircle } from "react-icons/bs";
 
 const ContactsList = ({ contacts }) => {
    const dispatch = useDispatch();
    return (  
       <Contacts>
-         {contacts.map(({name, phone, id}) => ( 
+         {contacts.map(({name, number, id}) => ( 
             <li key={nanoid()}>
-               <p><BsPersonCircle/> {name} : {phone}</p>
+               <p><BsPersonCircle/> {name} : {number}</p>
                <button type='button' onClick={()=> dispatch(deleteContact(id))}>
                   <BsFillTrashFill/>
                </button>
@@ -25,7 +25,7 @@ ContactsList.propTypes = {
    removeContact: PropTypes.func,
    contacts: PropTypes.arrayOf(PropTypes.shape({
       name: PropTypes.string.isRequired,
-      phone: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
       id: PropTypes.string.isRequired,
    }))
 }

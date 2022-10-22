9d1874bf158de01b66158ef5045338e0e544c270
Darya <daryabobur@gmail.com>
2022-10-22 22:22:55 +0300

    Create selectors.js

---
+++ src/redux/usersAuth/selectors.js
@@ -0,0 +1,5 @@
+export const getIsLoggedIn = state => state.users.user.isLoggedIn;
+
+export const getUser = state => state.users.user.name;
+
+export const getIsRefreshing = state => state.users.user.isRefreshing;

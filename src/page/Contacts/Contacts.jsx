e9fc8720d424d563c227c5128fafe45fc68e6b74
Darya <daryabobur@gmail.com>
2022-10-22 22:22:28 +0300

    Update Contacts.jsx

--- src/page/Contacts/Contacts.jsx
+++ src/page/Contacts/Contacts.jsx
@@ -2,7 +2,16 @@ import ContactForm from '../../components/ContactsForm/ContactsForm';
 import Filter from '../../components/Filter/Filter';
 import { FcPhoneAndroid } from "react-icons/fc";
 import { Title, Subtitle } from '../../components/App/AppStyled';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from 'redux/contacts/operations';
+
+
 const Contacts = () => {
+const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 return (
     <>
     

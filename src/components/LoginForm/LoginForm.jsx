078731b051489f5cdf1519be2a23367f615a8138
Darya <daryabobur@gmail.com>
2022-10-22 22:22:13 +0300

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,20 @@
-
+import { useDispatch } from 'react-redux';
+import { logInUser } from 'redux/usersAuth/operations';
 
 const LoginForm = () => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        const formData = evt.target;
+        dispatch(logInUser({
+            email: formData.elements.email.value,
+            password: formData.elements.password.value,
+        }))
+         formData.reset();   
+    }
 return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <label>Email
             <input type="email" name="email" />
         </label>

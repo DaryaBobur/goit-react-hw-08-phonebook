0bda3552f07685071522c392c52f0895f79672b7
Darya <daryabobur@gmail.com>
2022-10-22 22:22:16 +0300

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,10 +1,23 @@
-
+import { useDispatch } from 'react-redux';
+import { registerUser } from 'redux/usersAuth/operations';
 
 const RegisterForm = () => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        const formData = evt.currentTarget;
+        dispatch(registerUser({
+            name: formData.elements.name.value,
+            email: formData.elements.email.value,
+            password: formData.elements.password.value,
+        }))
+         formData.reset();   
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="">Name
-            <input type="text" name="name" />
+            <input type="name" name="name"/>
             </label>
             <label>Email
             <input type="email" name="email" />

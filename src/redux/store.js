354cc2256a58bcbb529105774e8e4838e385d501
Darya <daryabobur@gmail.com>
2022-10-23 19:46:49 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,41 @@
-import { configureStore,} from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
 import { filtersSlice } from 'redux/contacts/filterSlice';
 import { contactsSlice } from 'redux/contacts/contactsSlice';
 import { usersAuthSlice } from './usersAuth/usersAuthSlice';
-// getDefaultMiddleware 
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
 
-// const middleware = [
-//   ...getDefaultMiddleware({
-//     serializableCheck: {
-//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//     },
-//   }),
-// ];
-// const usersPersistConfig = {
-//   key: 'users',
-//   storage,
-//   whitelist: ['token'],
-// };
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const middleware = [
+  ...getDefaultMiddleware ({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+const usersAuthPersistConfig = {
+  key: 'users',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
-    users: usersAuthSlice.reducer,
+    users: persistReducer(usersAuthPersistConfig, usersAuthSlice.reducer),
     contacts: contactsSlice.reducer,
     filter: filtersSlice.reducer
   },
+  middleware,
+  // devTools: process.env.NODE_ENV === 'development',
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);

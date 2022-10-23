533051c061763f4ac0fe0d8ec019cc5c8403c6c4
Darya <daryabobur@gmail.com>
2022-10-23 19:46:38 +0300

    Create Layout.jsx

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,23 @@
+import AppBar from "components/AppBar/AppBar"
+import Loader from "components/Loader/Loader";
+import { Suspense } from 'react';
+import { Outlet } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
+
+
+const Layout = () => {
+    return (
+        <>
+        <AppBar />
+        <Suspense fallback={<Loader/>}>
+            <Outlet/>
+        </Suspense>
+        <Toaster
+        position="top-right"
+        reverseOrder={false}
+      />
+        </>
+    )
+}
+
+export default Layout;

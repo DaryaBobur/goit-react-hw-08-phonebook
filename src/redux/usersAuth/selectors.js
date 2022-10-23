d8ceb3e798fe0fac11426afe9c65ea2e409ada48
Darya <daryabobur@gmail.com>
2022-10-23 19:46:55 +0300

    Update selectors.js

--- src/redux/usersAuth/selectors.js
+++ src/redux/usersAuth/selectors.js
@@ -1,5 +1,5 @@
 export const getIsLoggedIn = state => state.users.user.isLoggedIn;
 
-export const getUser = state => state.users.user.name;
+export const getUserEmail = state => state.users.user.email;
 
 export const getIsRefreshing = state => state.users.user.isRefreshing;

0d2ef16f20fef4271412e59e14f2b20bb4460bec
Darya <daryabobur@gmail.com>
2022-10-24 21:41:46 +0300

    Create PrivateRoute.jsx

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { getIsLoggedIn, getIsCurrentUser } from "redux/usersAuth/selectors";
+
+const PrivateRoute = ({ component, redirectTo='/'}) => {
+const isLoggedIn = useSelector(getIsLoggedIn);
+const isCurrentUser = useSelector(getIsCurrentUser);
+const isRedirected = !isLoggedIn && !isCurrentUser;
+return isRedirected ? <Navigate to={redirectTo} /> : component;
+}
+
+export default PrivateRoute;  

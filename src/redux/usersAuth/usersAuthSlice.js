71235e1e35f8c43d004d3be54e1214d6bffb5206
Darya <daryabobur@gmail.com>
2022-10-24 21:42:09 +0300

    Update usersAuthSlice.js

--- src/redux/usersAuth/usersAuthSlice.js
+++ src/redux/usersAuth/usersAuthSlice.js
@@ -1,19 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { registerUser, logInUser, logOutUser, currentUser } from './operations';
 const handlePending = state => {
-    state.isLoading = true;
+    state.user.isLoading = true;
   };
   
   const handleRejected = (state, action) => {
-    state.isLoading = false;
-    state.error = action.payload;
+    state.user.isLoading = false;
+    state.user.error = action.payload;
   };
 
   const initialState = {
     user: { name: null, email: null },
     token: null,
     isLoggedIn: false,
-    // isRefreshing: false,
+    isCurrentUser: false,
   };
 
  export const usersAuthSlice = createSlice({
@@ -24,31 +24,38 @@ const handlePending = state => {
         [registerUser.fulfilled](state, action) {
             state.user = action.payload.user;
             state.token = action.payload.token;
-            state.isLoggedIn = true;
+            state.user.isLoggedIn = true;
         },
         [registerUser.rejected]: handleRejected,
         [logInUser.pending]: handlePending,
           [logInUser.fulfilled](state, action) {
             state.user = action.payload.user;
             state.token = action.payload.token;
-            state.isLoggedIn = true;
+            state.user.isLoggedIn = true;
           },
           [logInUser.rejected]: handleRejected,
 
-          [logOutUser.pending]: handlePending,
+          [logOutUser.pending](state){
+            state.user.isLoading = true;
+          },
           [logOutUser.fulfilled](state) {
             state.user = { name: null, email: null };
             state.token = null;
-            state.isLoggedIn = false;
+            state.user.isLoggedIn = false;
+            state.user.isLoading = false;
           },
           [logOutUser.rejected]: handleRejected,
 
-          [currentUser.pending] : handlePending,
+          [currentUser.pending](state){
+            state.user.isCurrentUser = true;
+          },
           [currentUser.fulfilled](state, action) {
             state.user = action.payload;
-            state.isLoggedIn = true;
-            state.isRefreshing = false;
+            state.user.isLoggedIn = true;
+            state.user.isCurrentUser = false;
           },
-          [currentUser.rejected]: handleRejected,
+          [currentUser.rejected](state){
+            state.user.isCurrentUser = false;
+          },
     }
  }) 

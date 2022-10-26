2ba596459e495d09f33d1de8c7fea1072ab07cb5
Darya <daryabobur@gmail.com>
2022-10-26 20:07:16 +0300

    Update RegisterFormStyled.js

--- src/components/RegisterForm/RegisterFormStyled.js
+++ src/components/RegisterForm/RegisterFormStyled.js
@@ -1,33 +1,28 @@
 import styled from '@emotion/styled'
 
 export const Form = styled.form`
-
-display: flex;
-flex-direction: column;
-width: 250px;
-margin-right: auto;
-margin-left: auto;
-
-
-
-button {
-    display:flex;
+    display: flex;
+    flex-direction: column;
+    width: 250px;
     margin-right: auto;
     margin-left: auto;
-    margin-top: 10px;
-    color: #2a2a2a;
+
+    button {
+        display:flex;
+        margin-right: auto;
+        margin-left: auto;
+        margin-top: 10px;
+        color: #2a2a2a;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 1.15;
+        background-color: #cad7e3;
+        transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
     
-    font-weight: 700;
-    font-size: 14px;
-    line-height: 1.15;
-
-    background-color: #cad7e3;
-
-    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-   
-    :hover, :focus {
-        background-color: #a5bacd;
-        color: #544d4d;
+        :hover, 
+        :focus {
+            background-color: #a5bacd;
+            color: #544d4d;
+        }
     }
-}
 `

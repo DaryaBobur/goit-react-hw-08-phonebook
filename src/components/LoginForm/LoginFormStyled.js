af1c1f861383c3b8a3e94ab8728625bc52b8d298
Darya <daryabobur@gmail.com>
2022-10-26 20:07:03 +0300

    Update LoginFormStyled.js

--- src/components/LoginForm/LoginFormStyled.js
+++ src/components/LoginForm/LoginFormStyled.js
@@ -1,28 +1,29 @@
 import styled from '@emotion/styled'
 
 export const Form = styled.form`
-width: 250px;
+    width: 250px;
 
-margin-right: auto;
-margin-left: auto;
-
-margin-bottom: 30px;
-
-button {
-    display:flex;
     margin-right: auto;
-    margin-left: auto; 
-    margin-top: 10px;
-    color: #2a2a2a;
-    font-weight: 700;
-    font-size: 14px;
-    line-height: 1.15;
-    background-color: #cad7e3;
-    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-   
-    :hover, :focus {
-        background-color: #a5bacd;
-        color: #544d4d;
+    margin-left: auto;
+
+    margin-bottom: 30px;
+
+    button {
+        display:flex;
+        margin-right: auto;
+        margin-left: auto; 
+        margin-top: 10px;
+        color: #2a2a2a;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 1.15;
+        background-color: #cad7e3;
+        transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    
+        :hover, 
+        :focus {
+            background-color: #a5bacd;
+            color: #544d4d;
+        }
     }
-}
 `

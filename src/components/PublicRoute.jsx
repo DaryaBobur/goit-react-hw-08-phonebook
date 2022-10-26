2f793af1ac96c9102cad8e30e5e5fccd1234725e
Darya <daryabobur@gmail.com>
2022-10-26 20:07:11 +0300

    Update PublicRoute.jsx

--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -3,9 +3,8 @@ import { Navigate } from 'react-router-dom';
 import { getIsLoggedIn } from "redux/usersAuth/selectors";
 import { useSelector } from "react-redux";
 
-
 const PublicRoute = ({ component, redirectTo = '/' }) => {
-    const isLoggedIn = useSelector(getIsLoggedIn);
+  const isLoggedIn = useSelector(getIsLoggedIn);
   return isLoggedIn ? <Navigate to={redirectTo} /> : component;
 };
 

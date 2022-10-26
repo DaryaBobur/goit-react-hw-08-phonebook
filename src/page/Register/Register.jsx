f8b533b22c94327fb30fb7f16f80ecb34353a00a
Darya <daryabobur@gmail.com>
2022-10-26 20:07:32 +0300

    Update Register.jsx

--- src/page/Register/Register.jsx
+++ src/page/Register/Register.jsx
@@ -2,13 +2,17 @@ import RegisterForm from "components/RegisterForm/RegisterForm";
 import { Container } from "components/UniversalStyles/ContainerStyled";
 import { Title } from "components/UniversalStyles/TitleStyled";
 import SupervisorAccountSharpIcon from '@mui/icons-material/SupervisorAccountSharp';
+
 const Register = () => {
 return (
     <Container>
-    <Title>Registration Page <SupervisorAccountSharpIcon sx={{ fontSize: 40 }}/></Title>
-    
-    <RegisterForm/>
-
+        <Title>
+            Registration Page 
+            <SupervisorAccountSharpIcon 
+                sx={{ fontSize: 40 }}
+            />
+        </Title>
+        <RegisterForm/>
     </Container>
 )
 }

4942db010a8d356309a9efed2330e79f8bae780b
Darya <daryabobur@gmail.com>
2022-10-26 20:07:36 +0300

    Update operations.js

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://634e58dc4af5fdff3a595a69.mockapi.io";
-
 export const fetchContacts = createAsyncThunk(
     "contacts/fetchAll", async (_, thunkAPI) => {
         try {

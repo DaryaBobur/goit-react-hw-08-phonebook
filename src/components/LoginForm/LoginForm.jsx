97e8bf81876f9188dfedcd3fbb3658fa938d4c79
Darya <daryabobur@gmail.com>
2022-10-26 20:07:02 +0300

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -6,55 +6,57 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import MailSharpIcon from '@mui/icons-material/MailSharp';
-
 import LockPersonSharpIcon from '@mui/icons-material/LockPersonSharp';
+
 const LoginForm = () => {
     const dispatch = useDispatch();
 
-    const handleSubmit = (evt) => {
+    const handleSubmit = evt => {
         evt.preventDefault();
         const formData = evt.target;
         dispatch(logInUser({
             email: formData.elements.email.value,
             password: formData.elements.password.value,
         }))
-         formData.reset();   
+        formData.reset();   
     }
-return (
-    <Form onSubmit={handleSubmit}>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-<MailSharpIcon sx={{mr: 1, my: 0.5, color: '#2a2a2a' }}/>
-<TextField
-     margin="dense"
-      size="small"
-      label="Email" 
-      variant="outlined"
-      type="email"
-      name="email"
-        required
-     
-      />
-   </Box>
 
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-<LockPersonSharpIcon sx={{mr: 1, my: 0.5, color: '#2a2a2a' }}/>
-<TextField 
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <MailSharpIcon 
+                    sx={{mr: 1, my: 0.5, color: '#2a2a2a' }}
+                />
+                
+                <TextField
+                    margin="dense"
+                    size="small"
+                    label="Email" 
+                    variant="outlined"
+                    type="email"
+                    name="email"
+                    required  
+                />
+            </Box>
 
-      size="small"
-      label="Password" 
-      variant="outlined"
-      margin="dense"
-      
-      type="password"
-      name="password"
-      required
-      />
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <LockPersonSharpIcon 
+                    sx={{mr: 1, my: 0.5, color: '#2a2a2a' }}
+                />
 
-</Box>
-      <Button variant="contained" type="submit">Log In</Button>
-
-    </Form>
-)
+                <TextField 
+                    size="small"
+                    label="Password" 
+                    variant="outlined"
+                    margin="dense"    
+                    type="password"
+                    name="password"
+                    required
+                />
+            </Box>
+            <Button variant="contained" type="submit">Log In</Button>
+        </Form>
+    )
 }
 
 LoginForm.propTypes = {

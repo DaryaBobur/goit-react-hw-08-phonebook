e29c1cb7e4f542075271ed964bac2153f41ba771
Darya <daryabobur@gmail.com>
2022-10-26 20:06:58 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,12 +4,12 @@ import { LoaderStyled } from './LoaderStyled';
 const Loader = () => {
     return (    
         <LoaderStyled>
-        <ThreeCircles  
-            visible={true} 
-            height="50" 
-            width="50"
-            color='#b3d0e9'
-        />
+            <ThreeCircles  
+                visible={true} 
+                height="50" 
+                width="50"
+                color='#b3d0e9'
+            />
         </LoaderStyled>
     )
 }

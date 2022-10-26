b49d6ceab9bbfa4f42cdfb56a76e444ecf8b54df
Darya <daryabobur@gmail.com>
2022-10-26 20:06:40 +0300

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,14 +1,16 @@
-
 import { AuthNavList, AuthNavItem, AuthNavLink } from "./AuthNavStyled";
 
-
 const AuthNav = () => {
-return (
-    <AuthNavList>
-        <AuthNavItem><AuthNavLink to="/register" end>Register</AuthNavLink></AuthNavItem>
-        <AuthNavItem><AuthNavLink to="/login">Log In</AuthNavLink></AuthNavItem>
-    </AuthNavList>
-)
+    return (
+        <AuthNavList>
+            <AuthNavItem>
+                <AuthNavLink to="/register" end>Register</AuthNavLink>
+            </AuthNavItem>
+            <AuthNavItem>
+                <AuthNavLink to="/login">Log In</AuthNavLink>
+            </AuthNavItem>
+        </AuthNavList>
+    )
 } 
 
 export default AuthNav;

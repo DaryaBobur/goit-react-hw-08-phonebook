44860f58817995a9f0ffdfd2e980202c697d72ce
Darya <daryabobur@gmail.com>
2022-10-26 16:54:06 +0300

    Update Home.jsx

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,11 +1,10 @@
-import { FcTwoSmartphones } from "react-icons/fc";
 import { Container } from "./HomeStyled";
-
+import SupervisorAccountSharpIcon from '@mui/icons-material/SupervisorAccountSharp';
 
 const Home = () => {
     return (
         <Container>
-        <h1><FcTwoSmartphones size={40}/>Welcome Phonebook App</h1>
+        <h1><SupervisorAccountSharpIcon sx={{mr: 1, my: 0.5, fontSize: 45 }}/>Welcome Phonebook App</h1>
         </Container>
     )
 }

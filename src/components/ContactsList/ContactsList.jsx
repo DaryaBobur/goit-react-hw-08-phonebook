ea42a8f09af5986f0d35f75fbbcb89ffeb36d72b
Darya <daryabobur@gmail.com>
2022-10-26 20:06:50 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -3,7 +3,6 @@ import { nanoid } from 'nanoid';
 import { useDispatch } from 'react-redux';
 import { Contacts } from './ContactsListStyled';
 import { deleteContact } from '../../redux/contacts/operations'
-
 import IconButton from '@mui/material/IconButton'
 import AccountCircleSharpIcon from '@mui/icons-material/AccountCircleSharp';
 import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
@@ -15,8 +14,11 @@ const ContactsList = ({ contacts }) => {
          {contacts.map(({ name, number, id }) => ( 
             <li key={nanoid()}>
                <p><AccountCircleSharpIcon/> {name} : {number}</p>
-               <IconButton aria-label="delete" size="small" type='button' onClick={()=> dispatch(deleteContact(id))}>
-                 <DeleteSharpIcon fontSize="small" />
+               <IconButton 
+                  size="small" 
+                  type='button' 
+                  onClick={()=> dispatch(deleteContact(id))}>
+                     <DeleteSharpIcon fontSize="small" />
                </IconButton>
             </li>
          ))}

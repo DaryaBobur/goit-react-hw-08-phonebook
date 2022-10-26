aac2d621ed75cbc7c4909575cce7acce649821f0
Darya <daryabobur@gmail.com>
2022-10-26 20:06:42 +0300

    Update AuthNavStyled.js

--- src/components/AuthNav/AuthNavStyled.js
+++ src/components/AuthNav/AuthNavStyled.js
@@ -2,28 +2,29 @@ import { NavLink } from 'react-router-dom';
 import styled from '@emotion/styled'
 
 export const AuthNavList = styled.ul`
-display: flex;
-align-items: center;
-gap: 20px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
 `
 
 export const AuthNavItem = styled.li`
-list-style: none;
+  list-style: none;
 `
 
 export const AuthNavLink = styled(NavLink)`
-padding-top: 10px;
-padding-bottom: 10px;
-font-weight: 700;
-font-size: 16px;
-color: #2a2a2a;
-text-decoration: none;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-weight: 700;
+  font-size: 16px;
+  color: #2a2a2a;
+  text-decoration: none;
 
   &.active {
-      color: #6495ad;
+    color: #6495ad;
   }
 
-  :hover, :focus {
+  :hover, 
+  :focus {
     color: #a0d2eb;
   } 
 `

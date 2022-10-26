0f1a9a677de6689f10301449e0f2ed006b161496
Darya <daryabobur@gmail.com>
2022-10-26 20:07:07 +0300

    Update NavigationStyled.js

--- src/components/Navigation/NavigationStyled.js
+++ src/components/Navigation/NavigationStyled.js
@@ -2,31 +2,30 @@ import styled from '@emotion/styled'
 import { NavLink } from "react-router-dom";
 
 export const NavList = styled.ul`
-display: flex;
-gap: 10px;
-padding: 0;
+  display: flex;
+  gap: 10px;
+  padding: 0;
 `
 
 export const NavItem = styled.li`
-display: flex;
-list-style: none;
-
+  display: flex;
+  list-style: none;
 `
 
 export const NavLinkStyled = styled(NavLink)`
-padding-top: 10px;
-padding-bottom: 10px;
-font-weight: 700;
-font-size: 18px;
-color: #2a2a2a;
-text-decoration: none;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-weight: 700;
+  font-size: 18px;
+  color: #2a2a2a;
+  text-decoration: none;
 
   &.active {
       color: #6495ad;
   }
 
-  :hover, :focus {
+  :hover, 
+  :focus {
     color: #a0d2eb;
   } 
 `
-

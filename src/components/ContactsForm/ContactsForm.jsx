fe43769366d2fc8227e4bf3be70914dbb9d20ef4
Darya <daryabobur@gmail.com>
2022-10-26 20:06:44 +0300

    Update ContactsForm.jsx

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -2,15 +2,15 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { nanoid } from 'nanoid';
-import { Form, ContainerForm } from './ContactsFormStyled';
+import { Form } from './ContactsFormStyled';
 import { addContact} from 'redux/contacts/operations';
-
-import PersonAddAltSharpIcon from '@mui/icons-material/PersonAddAltSharp';
-import PhoneIphoneSharpIcon from '@mui/icons-material/PhoneIphoneSharp';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-const ContactForm = () =>  {
+import Box from '@mui/material/Box';
+import PersonAddAltSharpIcon from '@mui/icons-material/PersonAddAltSharp';
+import PhoneIphoneSharpIcon from '@mui/icons-material/PhoneIphoneSharp';
 
+const ContactForm = () =>  {
   const dispatch = useDispatch();
 
   const [name, setName] = useState('');
@@ -49,44 +49,50 @@ const ContactForm = () =>  {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <ContainerForm>
-      <PersonAddAltSharpIcon sx={{ mr: 1, my: 0.5 }}/>
-      <TextField 
-      id={userInputId} 
-      size="small"
-      label="Name" 
-      variant="outlined"
-      value={name}
+
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <PersonAddAltSharpIcon 
+          sx={{ mr: 1, my: 0.5 }}
+        />
+        
+        <TextField 
+          id={userInputId} 
+          size="small"
+          label="Name" 
+          variant="outlined"
+          value={name}
+          type="text"
+          name="name"
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </Box>
       
-      type="text"
-      name="name"
-      onChange={handleChange}
-      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-      required
-      />
-</ContainerForm>
-<ContainerForm>
-    <PhoneIphoneSharpIcon sx={{ mr: 1, my: 0.5 }}/>
-      <TextField 
-      id={userInputId} 
-      size="small"
-      margin="normal"
-      label="Number" 
-      variant="outlined"
-      value={number}
-      type="tel"
-      name="number"
-      onChange={handleChange}
-      pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-      title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-      required
-      />
-</ContainerForm>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <PhoneIphoneSharpIcon 
+          sx={{ mr: 1, my: 0.5 }}
+        />
 
-<Button variant="contained" type="submit">Add contact</Button>
+        <TextField 
+          id={userInputId} 
+          size="small"
+          margin="normal"
+          label="Number" 
+          variant="outlined"
+          value={number}
+          type="tel"
+          name="number"
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </Box>
+
+      <Button variant="contained" type="submit">Add contact</Button>
     </Form>
-
   );
 }
 

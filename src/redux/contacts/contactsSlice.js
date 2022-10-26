6dd5a933ef27428d2bea9fc1f29be385835d18b5
Darya <daryabobur@gmail.com>
2022-10-26 20:07:34 +0300

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import toast from 'react-hot-toast';
 import { fetchContacts, addContact, deleteContact } from './operations';
-
+import { logOutUser } from 'redux/usersAuth/operations';
 const handlePending = state => {
   state.isLoading = true;
 };
@@ -15,8 +15,8 @@ export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
     items: [],
-      isLoading: false,
-      error: null,
+    isLoading: false,
+    error: null,
   },
   extraReducers: {
     [fetchContacts.pending]: handlePending,
@@ -38,14 +38,18 @@ export const contactsSlice = createSlice({
 
     [deleteContact.pending]: handlePending,
     [deleteContact.fulfilled](state, action) {
-
       state.isLoading = false;
       state.error = null;
       const index = state.items.findIndex((contact) => contact.id === action.meta.arg);
-        state.items.splice(index, 1);
-        toast.success(`Contact is deleted!`);
-   
+      state.items.splice(index, 1);
+      toast.success(`Contact is deleted!`);
     },
     [deleteContact.rejected]: handleRejected,
+
+    [logOutUser.fulfilled](state) {
+      state.items = [];
+      state.error = null;
+      state.isLoading = false;
+    },
   },
 });

e8667424b6bba404156f0ebf38bcde17710eff72
Darya <daryabobur@gmail.com>
2022-10-26 20:07:18 +0300

    Update TitleStyled.js

--- src/components/UniversalStyles/TitleStyled.js
+++ src/components/UniversalStyles/TitleStyled.js
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled'
 
 export const Title = styled.h2`
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 5px;
-        margin-bottom: 20px;
-        font-weight: 500;
+     display: flex;
+     align-items: center;
+     justify-content: center;
+     gap: 5px;
+     margin-bottom: 20px;
+     font-weight: 500;
 `
